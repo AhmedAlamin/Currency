13858619c8e6b34c9368b046073dcb897384ad8e
Ahmed Mohamed <engahmedegypto@gmail.com>
2023-08-26 01:41:38 +0300

    Finishing First Screen task logic.

--- app/src/main/java/com/ahmedalamin/currency/MainActivity.kt
+++ app/src/main/java/com/ahmedalamin/currency/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Info
@@ -26,6 +28,7 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -42,6 +45,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,6 +55,11 @@ import com.ahmedalamin.currency.ui.theme.CurrencyTheme
 import com.ahmedalamin.domain.entity.ApiRates
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -62,9 +71,6 @@ class MainActivity : ComponentActivity() {
     var data: ApiRates? = null
     private var allResponseData: ApiRates? = null
     private var listOfCurrencies: List<String>? = null
-
-
-
 
 
     @OptIn(ExperimentalMaterial3Api::class)
@@ -108,11 +114,10 @@ class MainActivity : ComponentActivity() {
 
 
 //        allResponseData = data // Update response with fetched items
-        listOfCurrencies = data.rates?.map { it.key } // Update selectedCurrency1 with fetched selection
+        listOfCurrencies =
+            data.rates?.map { it.key } // Update selectedCurrency1 with fetched selection
     }
 }
-
-
 
 
 fun calculateCurrencyAmount(
@@ -129,9 +134,6 @@ fun calculateCurrencyAmount(
         return null // Target currency not found in rates
     }
 }
-
-
-
 
 
 @Composable
@@ -202,7 +204,6 @@ fun IconTextButton(
 }
 
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CurrencyScreen(currenciesRates: ApiRates?) {
@@ -210,167 +211,213 @@ fun CurrencyScreen(currenciesRates: ApiRates?) {
     val listOfValuesOfCorrencies = currenciesRates?.rates?.values?.toList() ?: emptyList()
 
 
-
     var selectedCurrency1 by remember { mutableStateOf(listOfCurrencies.getOrNull(0)) }
     var selectedCurrency2 by remember { mutableStateOf(listOfCurrencies.getOrNull(0)) }
 
 
-            CurrencyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
+    CurrencyTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            var result by remember {
+                mutableStateOf(1.0)
+            }
+
+            var amount by remember {
+                mutableStateOf(1.0)
+            }
+
+
+
+            if (listOfCurrencies.isEmpty()) {
+                // Show loading indicator (e.g., ProgressBar)
+                CircularProgressIndicator(modifier = Modifier.wrapContentSize())
+            } else {
+                Column(
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
 
-                    var amount by remember {
-                        mutableStateOf(currenciesRates?.rates?.get(selectedCurrency1))
-                    }
-                    var result by remember {
-                        mutableStateOf(currenciesRates?.rates?.get(selectedCurrency2))
-                    }
 
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        verticalAlignment = Alignment.Top,
+                        modifier = Modifier
+                            .padding(top = 16.dp)
+                    ) {
 
-                    if (listOfCurrencies.isEmpty()) {
-                        // Show loading indicator (e.g., ProgressBar)
-                        CircularProgressIndicator(modifier = Modifier.wrapContentSize())
-                    } else {
                         Column(
-                            verticalArrangement = Arrangement.SpaceEvenly,
-                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceBetween,
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
+                            Text(text = "FROM")
 
+                            Spacer(modifier = Modifier.size(16.dp))
 
-                            Row(
-                                horizontalArrangement = Arrangement.SpaceAround,
-                                verticalAlignment = Alignment.Top,
-                                modifier = Modifier
-                                    .padding(top = 16.dp)
-                            ) {
+                            if (listOfCurrencies.isEmpty()) {
+                                // Show loading indicator (e.g., ProgressBar)
+                                CircularProgressIndicator(modifier = Modifier.wrapContentSize())
+                            } else {
 
-                                Column(
-                                    verticalArrangement = Arrangement.SpaceBetween,
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ) {
-                                    Text(text = "FROM")
+                                listOfCurrencies.let { it1 ->
+                                    selectedCurrency1?.let {
+                                        DropdownMenuComponent(
+                                            items = it1,
+                                            selectedItem = it,
+                                            onItemSelected = {
+                                                selectedCurrency1 = it
 
-                                    Spacer(modifier = Modifier.size(16.dp))
-
-                                    if (listOfCurrencies.isEmpty()) {
-                                        // Show loading indicator (e.g., ProgressBar)
-                                        CircularProgressIndicator(modifier = Modifier.wrapContentSize())
-                                    } else {
-
-                                        listOfCurrencies.let { it1 ->
-                                            selectedCurrency1?.let {
-                                                DropdownMenuComponent(
-                                                    items = it1,
-                                                    selectedItem = it,
-                                                    onItemSelected = { selectedCurrency1 = it },
-                                                    modifier = Modifier.wrapContentWidth()
-                                                )
-                                            }
-                                        }
+                                                result = convertCurrency(
+                                                    amount,
+                                                    selectedCurrency1!!,
+                                                    selectedCurrency2!!,
+                                                    currenciesRates!!
+                                                )!!
+                                            },
+                                            modifier = Modifier.wrapContentWidth()
+                                        )
                                     }
-
-
-                                    Spacer(modifier = Modifier.size(16.dp))
-
-
-
-                                    TextField(
-                                        value = amount.toString(), onValueChange = {
-                                         amount = it.toDouble()
-                                            result = amount?.let { it1 ->
-                                                currenciesRates?.rates?.get(selectedCurrency2)?.let { it2 ->
-                                                    calculateCurrencyAmount(
-                                                        currenciesRates,
-                                                        it2,selectedCurrency2!!,
-                                                        it1.toDouble())
-                                                }
-                                            }!!
-
-                                        }, modifier = Modifier
-                                            .size(100.dp)
-                                            .padding(16.dp)
-                                    )
-                                }
-
-
-                                IconTextButton(text = "Swich", icon = Icons.Default.Info, onClick = {})
-
-
-                                Column(
-                                    verticalArrangement = Arrangement.SpaceBetween,
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ) {
-                                    Text(text = "TO")
-
-                                    Spacer(modifier = Modifier.size(16.dp))
-
-
-                                    if (listOfCurrencies.isEmpty()) {
-                                        // Show loading indicator (e.g., ProgressBar)
-                                        CircularProgressIndicator(modifier = Modifier.wrapContentSize())
-                                    } else {
-
-                                        listOfCurrencies.let { it2 ->
-                                            selectedCurrency2?.let {
-                                                DropdownMenuComponent(
-                                                    items = it2,
-                                                    selectedItem = it,
-                                                    onItemSelected = { selectedCurrency2 = it },
-                                                    modifier = Modifier.wrapContentWidth()
-                                                )
-                                            }
-                                        }
-                                    }
-
-
-                                    Spacer(modifier = Modifier.size(16.dp))
-
-
-                                    Text(
-                                        text = result.toString() , Modifier
-                                            .background(Color.Magenta)
-                                            .size(100.dp)
-                                            .padding(16.dp)
-                                    )
-
                                 }
                             }
+
+
+                            Spacer(modifier = Modifier.size(16.dp))
+
+
+
+                            TextField(
+                                value = amount.toString(),
+                                onValueChange = {
+                                    amount = it.toDouble()
+                                    result = convertCurrency(
+                                        amount,
+                                        selectedCurrency1!!,
+                                        selectedCurrency2!!,
+                                        currenciesRates!!
+                                    )!!
+
+                                },
+                                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .width(100.dp)
+                                    .padding(16.dp)
+                            )
                         }
 
 
+                        IconTextButton(text = "Swich", icon = Icons.Default.Info, onClick = {
+
+                            var temp = selectedCurrency1
+                            selectedCurrency1 = selectedCurrency2
+                            selectedCurrency2 = temp
+
+                            result = convertCurrency(
+                                amount, selectedCurrency1!!, selectedCurrency2!!, currenciesRates!!
+                            )!!
+
+                        })
 
 
-
-
-
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceAround,
-                            verticalAlignment = Alignment.Top,
-                            modifier = Modifier
-                                .padding(top = 16.dp)
+                        Column(
+                            verticalArrangement = Arrangement.SpaceBetween,
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
+                            Text(text = "TO")
 
-                            IconTextButton(
-                                text = "Details",
-                                icon = Icons.Default.PlayArrow,
-                                onClick = {
+                            Spacer(modifier = Modifier.size(16.dp))
 
-                                })
+
+                            if (listOfCurrencies.isEmpty()) {
+                                // Show loading indicator (e.g., ProgressBar)
+                                CircularProgressIndicator(modifier = Modifier.wrapContentSize())
+                            } else {
+
+                                listOfCurrencies.let { it2 ->
+                                    selectedCurrency2?.let {
+                                        DropdownMenuComponent(
+                                            items = it2,
+                                            selectedItem = it,
+                                            onItemSelected = {
+                                                selectedCurrency2 = it
+                                                result = convertCurrency(
+                                                    amount,
+                                                    selectedCurrency1!!,
+                                                    selectedCurrency2!!,
+                                                    currenciesRates!!
+                                                )!!
+                                            },
+                                            modifier = Modifier.wrapContentWidth()
+                                        )
+                                    }
+                                }
+                            }
+
+
+                            Spacer(modifier = Modifier.size(16.dp))
+
+
+                            Text(
+                                text = roundDouble(result, 2).toString(),
+                                Modifier
+                                    .background(Color.Gray)
+                                    .size(100.dp)
+                                    .padding(16.dp)
+                            )
 
                         }
                     }
+                }
 
+
+
+
+
+
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.Top,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                ) {
+
+                    IconTextButton(
+                        text = "Details",
+                        icon = Icons.Default.PlayArrow,
+                        onClick = {
+
+                        })
 
                 }
             }
-                    }
 
 
+        }
+    }
+}
 
 
+fun convertCurrency(
+    amount: Double,
+    fromCurrency: String,
+    toCurrency: String,
+    exchangeRateData: ApiRates,
+): Double? {
+    val fromRate = exchangeRateData.rates[fromCurrency]
+    val toRate = exchangeRateData.rates[toCurrency]
 
+    if (fromRate != null && toRate != null) {
+        val baseAmount = amount / fromRate
+        return baseAmount * toRate
+    }
 
+    return null // Handle missing exchange rates
+}
 
+fun roundDouble(number: Double, digits: Int): Double {
+    return BigDecimal(number).setScale(digits, RoundingMode.HALF_UP).toDouble()
+}
